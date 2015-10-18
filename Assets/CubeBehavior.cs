55fe6062f24b07a09f7aea473f488c640f00b139
GaymeBoyColor <publicuser@asheclass-03.hampshire.edu>
2015-10-18 13:41:17 -0400

    Transportation Part 1

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+
+	void OnMouseDown () {
+
+		GameObject[] allCubes = GameObject.FindGameObjectsWithTag("Clickable Cube");
+		foreach (GameObject oneCube in allCubes) {
+			oneCube.GetComponent<Renderer>().material.color = Color.white;
+		}
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+}
---
+++ Assets/GameControllerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+	public GameObject cubePrefab;
+	public GameObject[] allCubes;
+	int numCubes = 16;
+
+	// Use this for initialization
+	void Start () {
+		allCubes = new GameObject[numCubes];
+		for (int i = 0; i < numCubes; i++) {
+			allCubes [i] = (GameObject)Instantiate (cubePrefab, new Vector3 (i * 2 - 14, 0, 10), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
